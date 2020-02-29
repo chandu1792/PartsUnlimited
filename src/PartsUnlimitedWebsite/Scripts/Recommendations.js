cfb1953feb06ab8930a59621f7ce268bb1b3e97e
chandu <41356294+chandu1792@users.noreply.github.com>
2020-02-29 14:19:15 +0530

    Update Recommendations.js

--- src/PartsUnlimitedWebsite/Scripts/Recommendations.js
+++ src/PartsUnlimitedWebsite/Scripts/Recommendations.js
@@ -6,8 +6,8 @@
         success: function (data) {
             $("#recommendations-panel").html(data);
         },
-        error: function (error) {
-            alert("Failed to get recommendations...");
+        error: function () {
+            echo alert("Failed to get recommendations...");
         },
     })
 }
